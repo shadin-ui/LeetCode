bbb8f5f2d0b020f901b0362f2cd56c0d51379ff2
shadin-ui <mshadink10@gmail.com>
2024-10-28 09:23:48 +0530

    single Number

---
+++ SingleNum.js
@@ -0,0 +1,14 @@
+function singleNumber(nums) {
+    let result = 0;
+    
+    for (let num of nums) {
+      result ^= num; 
+    }
+    
+    return result;
+  }
+  
+  console.log(singleNumber([2, 2, 1]));  
+  console.log(singleNumber([4, 1, 2, 1, 2])); 
+  console.log(singleNumber([1])); 
+  
