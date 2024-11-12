6483e249f134ec039c97fc5a764db62f9f9f5b4f
shadin-ui <mshadink10@gmail.com>
2024-11-12 10:06:51 +0530

    Perfect Number

---
+++ PerfectNum.js
@@ -0,0 +1,16 @@
+function checkPerfectNumber(num) {
+    if (num <= 1) return false;
+
+    let sum = 0;
+    
+    for (let i = 1; i <= Math.floor(num / 2); i++) {
+        if (num % i === 0) {
+            sum += i;
+        }
+    }
+
+    return sum === num;
+}
+
+console.log(checkPerfectNumber(28));
+console.log(checkPerfectNumber(7));
