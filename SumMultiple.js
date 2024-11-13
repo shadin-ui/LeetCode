fb246466d2b50d1135e5b222528596b7c24cb885
shadin-ui <mshadink10@gmail.com>
2024-11-13 10:56:13 +0530

    summultiple

---
+++ SumMultiple.js
@@ -0,0 +1,13 @@
+const sumOfMultiples = (n) => {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  };
+  
+  var ret = sumOfMultiples(7);
+  console.log(ret);  
+  
